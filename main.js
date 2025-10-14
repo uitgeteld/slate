0f4c8be2ceb2240336a1b6cf904bce1c726bb275
naive <swendewit07@gmail.com>
2025-10-14 21:47:07 +0200

    Slate commit

---
+++ main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow, Menu } = require('electron');
+const path = require("node:path")
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 900,
+    height: 600,
+    icon: path.join(__dirname, 'assets/icon256.ico'),
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+  Menu.setApplicationMenu(null);
+  win.loadFile('renderer/index.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
